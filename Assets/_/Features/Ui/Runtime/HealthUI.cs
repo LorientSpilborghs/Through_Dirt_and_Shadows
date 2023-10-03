8c8a747428dfb00673491768d97f24d909869592
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-10-03 13:57:31 +0200

    [UI] add health lost gauge visual

--- Assets/_/Features/Ui/Runtime/HealthUI.cs
+++ Assets/_/Features/Ui/Runtime/HealthUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,7 @@ namespace UIFeature.Runtime
     public class HealthUI : MonoBehaviour
     {
         public Slider m_greenHealthSlider;
+        public Slider m_redHealthSlider;
         public Slider m_upcomingHealthSlider;
         
         public void SetHealth(float currentResources, float maxResources)
@@ -13,18 +15,71 @@ namespace UIFeature.Runtime
             m_greenHealthSlider.maxValue = maxResources;
             m_greenHealthSlider.value = currentResources;
             m_upcomingHealthSlider.maxValue = maxResources;
-            m_upcomingHealthSlider.value = currentResources;
+            m_upcomingHealthSlider.value = currentResources;            
+            m_redHealthSlider.maxValue = maxResources;
+            m_redHealthSlider.value = currentResources;
             // _text.text = $"{currentResources}";
         }
 
         public void UpdateHealth(float currentResources, float totalUpcomingResources)
         {
             m_greenHealthSlider.value = currentResources;
-            m_upcomingHealthSlider.value = totalUpcomingResources;
+            // m_upcomingHealthSlider.value = totalUpcomingResources;
+
+            if (m_greenHealthSlider.value > m_redHealthSlider.value)
+            {
+                m_redHealthSlider.value = m_greenHealthSlider.value;
+            }
+            
+            if (_delayUpcomingHealthSlider is not null)
+            {
+                StopCoroutine(_delayUpcomingHealthSlider);
+            }
+            _delayUpcomingHealthSlider = StartCoroutine(DelayUpcomingHealthSlider(totalUpcomingResources));
+            
+            if (_delayRedHealthSlider is not null)
+            {
+                StopCoroutine(_delayRedHealthSlider);
+            }
+            _delayRedHealthSlider = StartCoroutine(DelayRedHealthSlider());
             // _text.text = $"{currentResources}";
         }
         
+        private IEnumerator DelayUpcomingHealthSlider(float totalUpcomingResources)
+        {
+            _upcomingTimer = 0;
+            while (m_upcomingHealthSlider.value < totalUpcomingResources)
+            {
+                m_upcomingHealthSlider.value = Mathf.Lerp(m_upcomingHealthSlider.value, totalUpcomingResources, _upcomingTimer/_upcomingHealthDelayDuration);
+                _upcomingTimer += Time.deltaTime;
+                yield return null;
+            }
+
+            _delayUpcomingHealthSlider = null;
+        }
+
+        private IEnumerator DelayRedHealthSlider()
+        {
+            _redTimer = 0;
+            while (m_redHealthSlider.value > m_greenHealthSlider.value)
+            {
+                m_redHealthSlider.value = Mathf.Lerp(m_redHealthSlider.value, m_greenHealthSlider.value, _redTimer/_redHealthDelayDuration);
+                _redTimer += Time.deltaTime;
+                yield return null;
+            }
+
+            _delayRedHealthSlider = null;
+        }
+
         
+        
+        [SerializeField] private float _redHealthDelayDuration = 10;
+        [SerializeField] private float _upcomingHealthDelayDuration = 1;
+        
+        private Coroutine _delayRedHealthSlider;
+        private Coroutine _delayUpcomingHealthSlider;
+        private float _upcomingTimer;
+        private float _redTimer;
         // private TextMeshProUGUI _text;
     }
 }
