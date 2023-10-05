85888cc296badae2e19dbb02a8331f6c6c906011
Lorient_Spilborghs <lorient.spilborghs@gmail.com>
2023-10-05 13:51:29 +0200

    [UI] update main menu

---
+++ Assets/_/Features/Ui/Runtime/VolumeSlider.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIFeature.Runtime
+{
+    public class VolumeSlider : MonoBehaviour
+    {
+        private enum VolumeType
+        {
+            MASTER,
+            MUSIC,
+            AMBIENCE,
+            SFX
+        }
+
+        private void Awake()
+        {
+            _volumeSlider = GetComponent<Slider>();
+        }
+
+        private void Update()
+        {
+            switch (_volumeType)
+            {
+                case VolumeType.MASTER:
+                    _volumeSlider.value = SettingsMenu.Instance.m_masterVolume;
+                    break;
+                case VolumeType.MUSIC:
+                    _volumeSlider.value = SettingsMenu.Instance.m_musicVolume;
+                    break;
+                case VolumeType.AMBIENCE:
+                    _volumeSlider.value = SettingsMenu.Instance.m_ambienceVolume;
+                    break;
+                case VolumeType.SFX:
+                    _volumeSlider.value = SettingsMenu.Instance.m_sfxVolume;
+                    break;
+            }
+        }
+
+        public void OnSliderValueChange()
+        {
+            switch (_volumeType)
+            {
+                case VolumeType.MASTER:
+                    SettingsMenu.Instance.m_masterVolume = _volumeSlider.value;
+                    break;
+                case VolumeType.MUSIC:
+                    SettingsMenu.Instance.m_musicVolume = _volumeSlider.value;
+                    break;
+                case VolumeType.AMBIENCE:
+                    SettingsMenu.Instance.m_ambienceVolume = _volumeSlider.value;
+                    break;
+                case VolumeType.SFX:
+                    SettingsMenu.Instance.m_sfxVolume = _volumeSlider.value;
+                    break;
+            }
+        }
+
+        [Header("Type")] [SerializeField] private VolumeType _volumeType;
+        
+        private Slider _volumeSlider;
+    }
+}
